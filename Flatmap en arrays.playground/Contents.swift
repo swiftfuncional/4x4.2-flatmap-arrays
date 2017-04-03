5abafd091146114d2727dea3e3bf1950c590c2cd
Swift Funcional <info@swiftfuncional.com>
2017-04-03 18:50:23 +0200

    Use map instead of guard let

--- Flatmap en arrays.playground/Contents.swift
+++ Flatmap en arrays.playground/Contents.swift
@@ -1,11 +1,7 @@
 let optionals: [Int?] = [2, 4, nil, 6, 8, nil, 10]
 
 let squares: [Int] = optionals.flatMap {
-	guard let x = $0 else {
-		return nil
-	}
-
-	return x*x
+	$0.map { x in x * x }
 }
 
 print(squares)
